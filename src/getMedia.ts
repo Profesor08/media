602b175d01c3c790f34a0ae096a7a3cc15fd39c6
Oleg Baltag <online7890@gmail.com>
2023-01-19 16:38:46 +0200

    fix conflicts with react hooks

---
+++ src/getMedia.ts
@@ -0,0 +1,15 @@
+const mediaMap = new Map<string, MediaQueryList>();
+
+export const getMedia = (query: string): MediaQueryList => {
+  const instance = mediaMap.get(query);
+
+  if (instance !== undefined) {
+    return instance;
+  }
+
+  const media = window.matchMedia(query);
+
+  mediaMap.set(query, media);
+
+  return media;
+};
