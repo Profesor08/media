91bc376b10d7150354b27016ad3f87811ecbd7f6
Oleg Baltag <online7890@gmail.com>
2023-01-19 17:03:14 +0200

    add old api compatibility

--- src/getMedia.ts
+++ src/getMedia.ts
@@ -9,6 +9,29 @@ export const getMedia = (query: string): MediaQueryList => {
 
   const media = window.matchMedia(query);
 
+  if (media.addEventListener === undefined) {
+    media.addEventListener = <K extends keyof MediaQueryListEventMap>(
+      event: K,
+      listener: (this: MediaQueryList, ev: MediaQueryListEventMap[K]) => void,
+      options?: boolean | AddEventListenerOptions,
+    ) => {
+      return media.addListener(listener);
+    };
+  }
+
+  if (media.removeEventListener === undefined) {
+    media.removeEventListener = <K extends keyof MediaQueryListEventMap>(
+      event: K,
+      listener: (
+        this: MediaQueryList,
+        ev: MediaQueryListEventMap[K],
+        options?: boolean | EventListenerOptions,
+      ) => void,
+    ) => {
+      return media.removeListener(listener);
+    };
+  }
+
   mediaMap.set(query, media);
 
   return media;
